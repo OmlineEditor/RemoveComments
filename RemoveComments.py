199dcc2c45483f73d2bcaafe01b56ac991d2b613
Anastasia <OmlineEditor@users.noreply.github.com>
2026-09-01 17:44:05 +0000

    solving the problem with comments
    solving the problem with comments https://github.com/junliu-lab/RemoveComments/issues/3
    
    After merging, update the version at https://packagecontrol.io/

--- RemoveComments.py
+++ RemoveComments.py
@@ -5,4 +5,8 @@ class RemoveFileCommentsCommand(sublime_plugin.TextCommand):
     def run(self, edit):
         comments = self.view.find_by_selector('comment')
         for region in reversed(comments):
-            self.view.erase(edit, region)
+            comment = self.view.substr(region)
+            line_breaks = ''.join(
+                character for character in comment if character in '\r\n'
+            )
+            self.view.replace(edit, region, line_breaks)
